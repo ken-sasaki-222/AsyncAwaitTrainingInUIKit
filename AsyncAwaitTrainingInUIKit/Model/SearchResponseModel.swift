187a1753752932663d3ed615f5b4af273b18bff9
佐々木　謙 <nkekisasa222@gmail.com>
2021-11-28 20:35:08 +0900

    add: 検索結果を受け取るModelを作成

---
+++ AsyncAwaitTrainingInUIKit/Model/SearchResponseModel.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResponseModel.swift
+//  AsyncAwaitTrainingInUIKit
+//
+//  Created by sasaki.ken on 2021/11/28.
+//
+
+import Foundation
+
+struct SearchResponseModel: Decodable {
+    var items: [Item]
+}
+
+struct Item: Decodable {
+    var name: String
+    var full_name: String
+}
