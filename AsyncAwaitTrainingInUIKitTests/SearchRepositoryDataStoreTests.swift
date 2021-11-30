58e52b143bd62467f78c46ecd2a62b15930f6490
佐々木　謙 <nkekisasa222@gmail.com>
2021-11-30 11:08:45 +0900

    add: テストファイル追加

--- AsyncAwaitTrainingInUIKitTests/SearchRepositoryDataStoreTests.swift
+++ AsyncAwaitTrainingInUIKitTests/SearchRepositoryDataStoreTests.swift
@@ -1,14 +1,13 @@
 //
-//  AsyncAwaitTrainingInUIKitTests.swift
+//  SearchRepositoryDataStoreTests.swift
 //  AsyncAwaitTrainingInUIKitTests
 //
-//  Created by sasaki.ken on 2021/11/28.
+//  Created by sasaki.ken on 2021/11/30.
 //
 
 import XCTest
-@testable import AsyncAwaitTrainingInUIKit
 
-class AsyncAwaitTrainingInUIKitTests: XCTestCase {
+class SearchRepositoryDataStoreTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
