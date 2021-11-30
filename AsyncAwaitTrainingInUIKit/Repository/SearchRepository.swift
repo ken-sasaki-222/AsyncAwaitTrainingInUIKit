23628d057ebc78f275a9524b06b3aae8bdaeab58
佐々木　謙 <nkekisasa222@gmail.com>
2021-11-30 10:55:43 +0900

    rafactor: エラーはcatchで受け取ってisEmptyはviewcontrollerでバリデーション

--- AsyncAwaitTrainingInUIKit/Repository/SearchRepository.swift
+++ AsyncAwaitTrainingInUIKit/Repository/SearchRepository.swift
@@ -10,14 +10,14 @@ import Foundation
 class SearchRepository: SearchRepositoryInterface {
     private let searchRepositoryDataStore = SearchRepositoryDataStore()
     
-    func searchGitHubRepository(searchText: String) async throws -> [Item]? {
-        if searchText.isEmpty {
-            return nil
-        }
+    func searchGitHubRepository(searchText: String) async throws -> [Item] {
         do {
             let response = try await searchRepositoryDataStore.searchRepositories(query: searchText)
-            let items = response?.items
+            let items = response.items
             return items
+        }
+        catch(let error) {
+            throw error
         }
     }
 }
